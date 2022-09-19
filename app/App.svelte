17a5c9aad469106ff53549a982a5f4f7b1de7a07
Andrea Canale <vivanco11.ac@gmail.com>
2022-09-19 17:52:39 +0200

    add summary page

--- app/App.svelte
+++ app/App.svelte
@@ -1,7 +1,18 @@
-<frame>
-    <Home />
-</frame>
+<tabView selectedIndex="{selectedIndex}" on:selectedIndexChange="{indexChange}">
+    <tabViewItem title="Summary">
+      <Home />
+    </tabViewItem>
+  
+    <tabViewItem title="CPU">
+      <label text="Content for Tab 2" />
+    </tabViewItem>
+  </tabView>
 
-<script lang="ts">
+<script>
     import Home from './components/Home.svelte'
+    let selectedIndex;
+    function indexChange(event) {
+        let newIndex = event.value
+        console.log('Current tab index: ' + newIndex)
+    }
 </script>
--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,24 +1,51 @@
 <page>
     <actionBar title="AndreKernelManager" />
-    <textView editable="{false}">
-        <formattedString>
-            <span text="Welcome to AndreKernelManager!"/>
-        </formattedString>
-    </textView>
-    <listView items="{frequency}">
-        <Template let:item>
-            <!-- Shows the list item label in the default color and style. -->
-            <label text="{item}" />
-        </Template>
-    </listView>
+    <flexboxLayout flexDirection="column">
+        <textView editable="{false}" height="10">
+            <formattedString>
+                <span text="Welcome to AndreKernelManager!\n"/>
+                <span text="Max CPU frequncy - governor:"/>
+            </formattedString>
+        </textView>
+        <listView items="{frequency}">
+            <Template let:item>
+                <label text="{item}" />
+            </Template>
+        </listView>
+        <textView editable="{false}">
+            <formattedString>
+
+                <span text="Total RAM: {(totalMemory / 1000000000).toFixed(3)} Gb\n" />
+                <span text="\n" />
+
+                <span text="Available RAM: {(ram_available / 1000000000).toFixed(3)} Gb\n"/>
+                <span text="\n" />
+
+                <span text="Phone Manufacturer: {DeviceInfo.systemManufacturer()}\n"/>
+                <span text="\n" />
+
+                <span text="Battery level: {batterylevel} %\n"/>
+                <span text="\n" />
+
+                <span text="Uptime and loadavg: {uptime}\n"/>
+                <span text="\n" />
+
+                <span text="Kernel: {kernel}"/>
+            </formattedString>
+        </textView>
+    </flexboxLayout>
+   
 </page>
 
 <script>
-    import { Template } from 'svelte-native/components'
-    let frequency = [];
+    import { Template } from 'svelte-native/components';
+    import { DeviceInfo } from "nativescript-dna-deviceinfo";
+
+    let frequency = [], batterylevel = 0, ram_available = 0, totalMemory = 0, uptime = "", kernel = "";
+    java.lang.Runtime.getRuntime().exec("su");
+
     function get_cpu_freq(id){
         var o = new java.lang.String();
-        
         var process = java.lang.Runtime.getRuntime().exec('cat /sys/devices/system/cpu/cpu' + id + '/cpufreq/scaling_cur_freq');
         var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
         var line = new java.lang.String();
@@ -26,13 +53,47 @@
         return o;
     }
 
-    function init(){
-        java.lang.Runtime.getRuntime().exec("su");
-        for(var i = 0; i < 8; i++){
-            frequency.push(get_cpu_freq(i));
-        }
+    function get_cpu_gov(id){
+        var o = new java.lang.String();
+        var process = java.lang.Runtime.getRuntime().exec('cat /sys/devices/system/cpu/cpu' + id + '/cpufreq/scaling_governor');
+        var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+        var line = new java.lang.String();
+        while((line=reader.readLine())!=null)o+=line;
+        return o;
     }
-    setTimeout(init, 500)
+
+    function get_uptime(){
+        var o = new java.lang.String();
+        var process = java.lang.Runtime.getRuntime().exec('uptime');
+        var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+        var line = new java.lang.String();
+        while((line=reader.readLine())!=null)o+=line;
+        return o;
+    }
+
+    function get_kernel(){
+        var o = new java.lang.String();
+        var process = java.lang.Runtime.getRuntime().exec('uname -a');
+        var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+        var line = new java.lang.String();
+        while((line=reader.readLine())!=null)o+=line;
+        return o;  
+    }
+
+    async function init(){
+        batterylevel = DeviceInfo.batteryLevel();
+        ram_available = DeviceInfo.freeMemory();
+        totalMemory = DeviceInfo.totalMemory();
+        frequency = [];
+        uptime = get_uptime();
+        kernel = get_kernel();
+        //console.log(get_uptime());
+        for(var i = 0; i < 8; i++){
+            frequency = frequency.concat("CPU " + i + ": " + get_cpu_freq(i) / 1000000 + " GHz - " + get_cpu_gov(i));
+        }
+        setTimeout(init, 500);
+    }
+
     init();
 </script>
 
