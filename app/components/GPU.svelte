e3043a9da47cbebdbccda3b71b3e4601d9cac23d
Andrea Canale <vivanco11.ac@gmail.com>
2022-09-20 23:36:50 +0200

    finish gpu part

--- app/components/GPU.svelte
+++ app/components/GPU.svelte
@@ -1,47 +1,50 @@
 <page>
     <flexboxLayout flexDirection="column">
 
-        <textView editable="{false}">
+        <label textWrap="{true}" class="text">
             <formattedString>
                 <span text="GPU Load: {load} %\n" />
                 <span text="Current governor: {current_gov}\n" />
+                <span text="Current frequency: {current_freq} mHz" />
             </formattedString>
-        </textView>
+        </label>
 
         <flexboxLayout alignItems="flex-start">
             <label text="Adrenoboost" class="icon_text"/>
             <switch bind:checked="{adrenoboost}" on:checkedChange={set_adrenoboost(adrenoboost)} class="fsync"/>
         </flexboxLayout>
-        
-        <!-- <flexboxLayout alignItems="flex-start">
-            <label text="Enable 600hz touch sampling(only oplus)" class="fsync"/>
-            <switch bind:checked="{touchrate}" on:checkedChange={touchrate_switch}/>
-        </flexboxLayout>
 
-        <flexboxLayout alignItems="flex-start">
-            <label text="Adrenoboost" class="fsync"/>
-            <switch bind:checked="{adrenoboost}" on:checkedChange={set_adrenoboost}/>
-        </flexboxLayout> -->
-        
+        <listPicker items="{governors}" selectedIndex="0" on:selectedIndexChange="{selectedIndexChanged}" />
+        <button text="Set governor" backgroundColor="#2497d1" on:tap="{set_gov(governor)}"/>
+        <button text="Set max frequency" backgroundColor="#2497d1" on:tap="{set_min_freq(min, max)}"/>
+        <button text="Set max frequency" backgroundColor="#2497d1" on:tap="{set_max_freq(min, max)}"/>
+
+
     </flexboxLayout>
 </page>
 
 <script lang="ts">
     import { onMount } from 'svelte';
     // @ts-ignore
-    import { get_adrenoboost, set_adrenoboost, get_load, get_gov } from '../lib/gpu.ts';
+    import { get_adrenoboost, set_adrenoboost, get_load, get_gov, get_all_gov, set_gov, get_min_freq, get_max_freq, get_cur_freq, set_min_freq, set_max_freq } from '../lib/gpu.ts';
 
-    let load = 0, current_gov = 0, adrenoboost = false;
+    let load = 0, current_gov = 0, adrenoboost = false, governors: Array<string> = [], governor = 0, current_freq = 0, min = 0, max = 0;
+
+    const selectedIndexChanged = (e: any) => governor = e.index;
 
     function load_refresh(){
         load = get_load();
+        current_freq = get_cur_freq() / 1000000;
         setTimeout(load_refresh, 1000);
     }
 
     onMount(() => {
         load_refresh();
+        governors = get_all_gov();
         current_gov = get_gov();
         adrenoboost = Boolean(parseInt(get_adrenoboost()));
+        min = get_min_freq();
+        max = get_max_freq();
     });
 </script>
 
@@ -52,5 +55,9 @@
     .icon_text{
         margin-left: 50px;
         margin-top: 50px;
+    }
+    .text{
+        margin-top: 20px;
+        margin-left: 50px;
     }
 </style>
--- app/lib/gpu.ts
+++ app/lib/gpu.ts
@@ -1,3 +1,5 @@
+import { prompt, alert } from '@nativescript/core/ui/dialogs';
+
 // java.lang.Runtime.getRuntime().exec('su') Get SU permission
 
 export function get_adrenoboost(){
@@ -53,6 +55,144 @@ export function get_gov(){
     let outputStream = new java.io.DataOutputStream(process.getOutputStream());
     // Maybe change this path?
     outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/governor\n");
+    outputStream.flush();
+    outputStream.writeBytes("exit\n");
+    outputStream.flush();
+    process.waitFor();
+    var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+    var line = new java.lang.String();
+    while((line = reader.readLine())!=null)o+=line;
+    return o;
+}
+
+export function get_all_gov(){
+    var o = new java.lang.String();
+    var process = java.lang.Runtime.getRuntime().exec('su');
+    let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+    // Maybe change this path?
+    outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/available_governors\n");
+    outputStream.flush();
+    outputStream.writeBytes("exit\n");
+    outputStream.flush();
+    process.waitFor();
+    var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+    var line = new java.lang.String();
+    while((line = reader.readLine())!=null)o+=line;
+    return o.split(" ");
+}
+
+export function set_gov(governor: number){
+    let all_gov = get_all_gov();
+    var o = new java.lang.String();
+    var process = java.lang.Runtime.getRuntime().exec('su');
+    let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+    // Maybe change this path?
+    outputStream.writeBytes(`echo "${all_gov[governor]}" > /sys/class/devfreq/3d00000.qcom,kgsl-3d0/governor\n`);
+    outputStream.flush();
+    outputStream.writeBytes("exit\n");
+    outputStream.flush();
+    process.waitFor();
+    var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+    var line = new java.lang.String();
+    while((line = reader.readLine())!=null)o+=line;
+    alert("Governor changed successfully");
+    return o.split(" "); 
+}
+
+export function get_min_freq(){
+    var o = new java.lang.String();
+    var process = java.lang.Runtime.getRuntime().exec('su');
+    let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+    // Maybe change this path?
+    outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/min_freq\n");
+    outputStream.flush();
+    outputStream.writeBytes("exit\n");
+    outputStream.flush();
+    process.waitFor();
+    var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+    var line = new java.lang.String();
+    while((line = reader.readLine())!=null)o+=line;
+    return o;
+}
+
+export function get_max_freq(){
+    var o = new java.lang.String();
+    var process = java.lang.Runtime.getRuntime().exec('su');
+    let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+    // Maybe change this path?
+    outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/max_freq\n");
+    outputStream.flush();
+    outputStream.writeBytes("exit\n");
+    outputStream.flush();
+    process.waitFor();
+    var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+    var line = new java.lang.String();
+    while((line = reader.readLine())!=null)o+=line;
+    return o;
+}
+
+export function set_min_freq(min: number, max: number){
+    prompt('Set frequency between ' + min + " and " + max, "")
+    .then(async res => {
+        if(!res.result){}else{
+            if(res.text == "" || parseInt(res.text) <= min || parseInt(res.text) >= max){
+                alert({
+                    title: "Error",
+                    message: "Invalid input!",
+                    okButtonText: "OK"
+                });
+            }else{
+                var process = java.lang.Runtime.getRuntime().exec('su');
+                let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+                outputStream.writeBytes('echo ' + res.text + " " + '> /sys/class/devfreq/3d00000.qcom,kgsl-3d0/min_freq\n');
+                outputStream.flush();
+                outputStream.writeBytes("exit\n");
+                outputStream.flush();
+                process.waitFor();
+                alert({
+                    title: "Success",
+                    message: "Min frequency set successfully!",
+                    okButtonText: "OK"
+                });
+            }
+        }
+    })
+}
+
+export function set_max_freq(min: number, max: number){
+    prompt('Set frequency between ' + min + " and " + max, "")
+    .then(async res => {
+        if(!res.result){}else{
+            if(res.text == "" || parseInt(res.text) <= min || parseInt(res.text) >= max){
+                alert({
+                    title: "Error",
+                    message: "Invalid input!",
+                    okButtonText: "OK"
+                });
+            }else{
+                var process = java.lang.Runtime.getRuntime().exec('su');
+                let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+                outputStream.writeBytes('echo ' + res.text + " " + '> /sys/class/devfreq/3d00000.qcom,kgsl-3d0/max_freq\n');
+                outputStream.flush();
+                outputStream.writeBytes("exit\n");
+                outputStream.flush();
+                process.waitFor();
+                alert({
+                    title: "Success",
+                    message: "Max frequency set successfully!",
+                    okButtonText: "OK"
+                });
+            }
+        }
+    })
+}
+
+export function get_cur_freq(){
+    var o = new java.lang.String();
+    var process = java.lang.Runtime.getRuntime().exec('su');
+    let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+    // Maybe change this path?
+    outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/cur_freq\n");
     outputStream.flush();
     outputStream.writeBytes("exit\n");
     outputStream.flush();
