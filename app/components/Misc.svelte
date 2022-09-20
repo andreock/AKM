b1307ba8909b7a1cec66abc9cbb718bf61f0e03d
Andrea Canale <vivanco11.ac@gmail.com>
2022-09-20 12:28:48 +0200

    Fix misc command

--- app/components/Misc.svelte
+++ app/components/Misc.svelte
@@ -13,7 +13,7 @@
 
         <flexboxLayout alignItems="flex-start">
             <label text="Adrenoboost" class="fsync"/>
-            <switch bind:checked="{adrenoboost}" on:checkedChange={touchrate_switch}/>
+            <switch bind:checked="{adrenoboost}" on:checkedChange={set_adrenoboost}/>
         </flexboxLayout>
         
     </flexboxLayout>
@@ -28,7 +28,7 @@
         var o = new java.lang.String();
         var process = java.lang.Runtime.getRuntime().exec('su');
         let outputStream = new java.io.DataOutputStream(process.getOutputStream());
-        outputStream.writeBytes("cat /sys/class/devfreq/1c00000.qcom,kgsl-3d0/adrenoboost\n");
+        outputStream.writeBytes("cat /sys/class/devfreq/3d00000.qcom,kgsl-3d0/adrenoboost\n");
         outputStream.flush();
         outputStream.writeBytes("exit\n");
         outputStream.flush();
@@ -36,8 +36,21 @@
         var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
         var line = new java.lang.String();
         while((line=reader.readLine())!=null)o+=line;
-        alert(o)
         return o;
+    }
+
+    function set_adrenoboost(){
+        var process = java.lang.Runtime.getRuntime().exec('su');
+        let outputStream = new java.io.DataOutputStream(process.getOutputStream());
+        if(!adrenoboost){
+            outputStream.writeBytes("echo 1 > /sys/class/devfreq/3d00000.qcom,kgsl-3d0/adrenoboost\n");
+        }else{
+            outputStream.writeBytes("echo 0 > /sys/class/devfreq/3d00000.qcom,kgsl-3d0/adrenoboost\n");
+        }
+        outputStream.flush();
+        outputStream.writeBytes("exit\n");
+        outputStream.flush();
+        process.waitFor();
     }
 
     function get_oplus_touchrate(){
@@ -97,7 +110,7 @@
 
     onMount(() => {
         touchrate = Boolean(parseInt(get_oplus_touchrate()[0]));
-        console.log(get_adrenoboost())
+        adrenoboost = Boolean(parseInt(get_adrenoboost()));
     }
     );
     
